4817f8ac0957731deeaa6b9fa37141de98d2bd34
VicenteIturrieta <vicente.iturrieta1901@alumnos.ubiobio.cl>
2023-05-10 00:32:24 -0400

    formulario-vicente

---
+++ frontend/src/pages/ContactoPage.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import ContactForm from '../components/ContactForm';
+import styles from './ContactoPage.module.css';
+
+export default function ContactoPage() {
+    return (
+        <div>
+            <h1>Formulario de contacto</h1>
+            <ContactForm />
+        </div>
+    );
+}
